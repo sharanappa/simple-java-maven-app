fc63cadcab011208e38569badf53685ec953fc7e
sharanappa <30830479+sharanappa@users.noreply.github.com>
2019-07-11 15:02:54 +0530

    Update App.java
    Demo Update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!...demo update";
+    private final String message = "Hello World!...Demo Update";
 
     public App() {}
 
